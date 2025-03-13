53c8810c90c5ba1c4ce57e56a524b0d872c806d3
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-03-13 09:17:36 +0000

    Finished BetterDecorador

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -7,7 +7,11 @@ namespace BetterDecorador
 
         private static void Main(string[] args)
         {
-            
+            string str = args[0];
+            char chr = args[1][0];
+            int count = Int32.Parse(args[2]);
+
+            Console.WriteLine(Decor(str, chr, count));
         }
         private static string Decor(string str, char chr, int count)
         {
